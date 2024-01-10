a9bd1ee5188780f424ceff4c432e8a94e937507c
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-01-10 13:37:47 -0300

    inserindo as imagens no componente tentativas via property binding

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
-  styleUrls: ['./tentativas.component.css']
+  styleUrls: ['./tentativas.component.css'],
 })
 export class TentativasComponent {
-
+  public coracaoVazio: string = '/assets/coracao_vazio.png';
+  public coracaoCheio: string = '/assets/coracao_cheio.png';
 }
