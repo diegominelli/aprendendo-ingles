a2c4869a573fa54ef3c167179a30e363a7c50051
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-01-10 14:40:43 -0300

    criando um mock do objeto frase

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,24 @@
+import { Frase } from '../shared/frase.model';
+
+const FRASES: Frase[] = [
+  {
+    fraseEng: 'I like to learn',
+    frasePtBr: 'Eu gosto de apreder',
+  },
+  {
+    fraseEng: 'I watch tv',
+    frasePtBr: 'Eu assisto tv',
+  },
+  {
+    fraseEng: 'How are you ?',
+    frasePtBr: 'Como vai você ?',
+  },
+  {
+    fraseEng: 'I eat bread',
+    frasePtBr: 'Eu como pão',
+  },
+  {
+    fraseEng: 'I like to ride a bike',
+    frasePtBr: 'Eu gosto de andar de bicicleta',
+  },
+];
