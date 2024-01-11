c1404983a59997b033567792d4c47547acdb9892
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-01-11 12:48:32 -0300

    desenvolvendo a logica das perguntas e respostas

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Frase } from '../shared/frase.model';
 import { FRASES } from './frases-mock';
 
@@ -7,11 +7,34 @@ import { FRASES } from './frases-mock';
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css'],
 })
-export class PainelComponent {
+export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES;
   public instrucao: string = 'Traduza a frase:';
+  public resposta?: string;
+
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
 
   constructor() {
-    console.log(this.frases);
+    this.rodadaFrase = this.frases[this.rodada];
+    // console.log(this.frases);
+  }
+
+  ngOnInit() {}
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+    // console.log(this.resposta);
+  }
+
+  public verificarResposta(): void {
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('Parabéns! A tradução está correta');
+      this.rodada++;
+
+      this.rodadaFrase = this.frases[this.rodada];
+    } else {
+      alert('Que pena a tradução está errada');
+    }
   }
 }
